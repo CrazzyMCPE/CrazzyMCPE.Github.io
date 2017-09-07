cee80fa25da5ec3e2e49a4b9ab050e7b50ae9f05
CrazzyMCPE <crazzygamermcpe@gmail.com>
2017-09-07 14:23:27 -0800

    Lista de cambios
    Versión 0.6.0
    (Beta 1)
    Cambiado el sistema del banner
    {anteriormente se usaba un solo archivo png como banner, el nuevo sistema usa diferentes archivos png para el background del banner y el texto se aplíca con código)}
    Cambiado el estilo de fuente

---
+++ js/Menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("1000");
+    });
+});
+
+
+
+$( ".selector" ).slider({
+  slide: function( event, ui ) {}
+});
